12427276da25a08ea3c2de431d8da6da971394aa
Itamar <iaxelrad@gmail.com>
2024-05-18 19:29:12 +0300

    Initial Commit

---
+++ ToDoListSwiftUI/ToDoListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListSwiftUIApp.swift
+//  ToDoListSwiftUI
+//
+//  Created by Itamar Axelrad on 18/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
